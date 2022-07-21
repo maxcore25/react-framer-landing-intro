30e8bc91768ca0bb4433784ba5955540c6c96928
maxcore25 <mkorobov2002@mail.ru>
2022-07-21 18:52:13 +0300

    Main App JSX Pasted

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './sass/main.scss';
+
+// Components
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Loader from './components/Loader';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loading
+      ? document.querySelector('body').classList.add('loading')
+      : document.querySelector('body').classList.remove('loading');
+  }, [loading]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loading ? (
+        <Loader setLoading={setLoading} />
+      ) : (
+        <>
+          <Header />
+          <Banner />
+          {!loading && (
+            <div className='transition-image final'>
+              <img src={process.env.PUBLIC_URL + `/images/image-2.jpg`} />
+            </div>
+          )}
+        </>
+      )}
+    </>
   );
 }
 
